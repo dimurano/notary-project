13ea91868d333b869437763252489f5fdf75da28
RICHARD FRANCO <33595611+dimurano@users.noreply.github.com>
2026-09-25 16:14:48 -0400

    Change read_root to an async function

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from auth import create_access_token, verify_password, get_current_user_email
 
 from fastapi import FastAPI
 app = FastAPI()
-@app.get("/")
-def read_root():
+
+@app.get("/") 
+async def read_root(): 
     return {"status": "healthy", "message": "Welcome to Notarial Solutions API"}
 
 app = FastAPI(title="Notary Core Service")
